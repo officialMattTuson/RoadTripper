17e9bd786675391715006aebcfb5d7500a57c10b
Matthew Tuson <matthew.tuson@valocityglobal.com>
2023-03-29 21:59:49 +1300

    footer now receives and displays countries list

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,19 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Location } from '@angular/common';
+import { AppService } from 'src/app/app.service';
+import { catchError, EMPTY, map } from 'rxjs';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent {
 
   previousUrlString = '';
   currentUrl = window.location.href;
+  locations$ = this.appService.getLocations().pipe(
+    catchError(error => {
+    console.log(error)
+    return EMPTY;
+  }),
+    map(locations => {
+      const listOfLocations: string[] = [];
+      return locations.map(location => {
+        if (listOfLocations.includes(location.country)) {
+          return listOfLocations;
+        }
+        listOfLocations.push(location.country);
+        return listOfLocations
+      })
+    }),
+  );
 
-  constructor(public location: Location) {
-  }
-  ngOnInit(): void {
+  constructor(public location: Location,
+              private appService: AppService) {
   }
 
 }
