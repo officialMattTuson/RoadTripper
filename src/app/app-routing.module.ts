d8aa4d2ee623678016d2757ffd33a146275e4d5c
cornrowsMatt <mtuson1414@gmail.com>
2024-03-08 01:25:18 +1300

    new booking pages assigned routes to house the component view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,14 @@ import { CarHireComponent } from './pages/car-hire/car-hire.component';
 import { LocationsComponent } from './pages/locations/locations.component';
 import { LodgePackageComponent } from './pages/lodge-package/lodge-package.component';
 import { ServicesComponent } from './pages/services/services.component';
+import { BookingRequestComponent } from './pages/bookings/booking-request/booking-request.component';
+import { BookingConfirmationComponent } from './pages/bookings/booking-confirmation/booking-confirmation.component';
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: 'home'},
   { path: 'home', component: HomeComponent, title: 'Road Tripper' },
+  { path: 'booking', component: BookingRequestComponent, title: 'Booking Request' },
+  { path: 'booking/:id', component: BookingConfirmationComponent, title: 'Booking Confirmation' },
   { path: 'car-hire', component: CarHireComponent, title: 'Our Fleet' },
   { path: 'locations', component: LocationsComponent, title: 'Locations' },
   { path: 'lodge-package', component: LodgePackageComponent, title: 'Lodge Packages' },
