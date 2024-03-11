80f9efdc6552c21281f1cfab3784b4e23cb6d3ff
Matthew Tuson <mtuson1414@gmail.com>
2024-03-11 18:00:25 +1300

    duplicated instances of car cards in car hire page and availablity popup combined in its own component, have ng class and ngSelect tools to help differentiate the configs

--- src/app/car-card/car-card.component.ts
+++ src/app/car-card/car-card.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { BaseCardComponent } from '../base-card/base-card.component';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { BookingsService } from '../services/bookings.service';
+import { Car } from '../interfaces/interfaces';
 
 @Component({
   selector: 'app-car-card',
   templateUrl: './car-card.component.html',
-  styleUrls: ['./car-card.component.scss']
+  styleUrls: ['./car-card.component.scss'],
 })
-export class CarCardComponent implements OnInit {
+export class CarCardComponent extends BaseCardComponent implements OnInit {
+  @Input() car!: Car;
+  @Input() showHideCarouselCarClass!: object;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    protected override readonly bookingsService: BookingsService,
+    protected override readonly dialog: MatDialog,
+    protected override readonly router: Router
+  ) {
+    super(bookingsService, dialog, router);
   }
 
+  override ngOnInit(): void {}
 }
