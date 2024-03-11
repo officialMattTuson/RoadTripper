257bf573f7065944423a5ec1d208587e449c1fa5
Matthew Tuson <mtuson1414@gmail.com>
2024-03-11 18:24:08 +1300

    addressed type issue with car cards, added boolean to stop function triggering many times

--- src/app/car-card/car-card.component.ts
+++ src/app/car-card/car-card.component.ts
@@ -3,7 +3,7 @@ import { BaseCardComponent } from '../base-card/base-card.component';
 import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { BookingsService } from '../services/bookings.service';
-import { Car } from '../interfaces/interfaces';
+import { AvailableCarFullModel, Car } from '../interfaces/interfaces';
 
 @Component({
   selector: 'app-car-card',
@@ -11,8 +11,10 @@ import { Car } from '../interfaces/interfaces';
   styleUrls: ['./car-card.component.scss'],
 })
 export class CarCardComponent extends BaseCardComponent implements OnInit {
-  @Input() car!: Car;
+  @Input() car!: Car | AvailableCarFullModel;
   @Input() showHideCarouselCarClass!: object;
+
+  hasExecuted: boolean = false;
 
   constructor(
     protected override readonly bookingsService: BookingsService,
@@ -23,4 +25,11 @@ export class CarCardComponent extends BaseCardComponent implements OnInit {
   }
 
   override ngOnInit(): void {}
+
+  isAvailableCarFullModel(
+    car: Car | AvailableCarFullModel
+  ): car is AvailableCarFullModel {
+    this.hasExecuted = true;
+    return (car as AvailableCarFullModel).numberOfCars !== undefined;
+  }
 }
