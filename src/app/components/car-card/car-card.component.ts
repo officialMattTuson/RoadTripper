8d8f20aa40d77a31c734fe488d0c2fdc255db3c1
cornrowsMatt <mtuson1414@gmail.com>
2024-04-17 18:16:37 +1200

    updated ts rules, moved car-card into components folder

---
+++ src/app/components/car-card/car-card.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+import { Car, AvailableCarFullModel } from 'src/app/interfaces/interfaces';
+
+@Component({
+  selector: 'app-car-card',
+  templateUrl: './car-card.component.html',
+  styleUrls: ['./car-card.component.scss'],
+})
+export class CarCardComponent {
+  @Input() car!: Car | AvailableCarFullModel;
+  @Input() showHideCarouselCarClass!: object;
+
+  isAvailableCarFullModel(
+    car: Car | AvailableCarFullModel
+  ): car is AvailableCarFullModel {
+    return (car as AvailableCarFullModel).numberOfCars !== undefined;
+  }
+}
